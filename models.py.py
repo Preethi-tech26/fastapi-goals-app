3199ba33fe474becd7fefb053bc5d1bcc9a153c9
Preethi Iyengar <preethirv2@gmail.com>
2026-06-26 12:43:52 -0400

    Remove practice files from repo

--- models.py.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Goal(Base):
-    __tablename__ = "goals"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    completed = Column(Integer, default=0)
